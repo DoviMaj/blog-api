25f8f504c96a8a57ce3f26d332b1619c78b54179
dovimaj <dovimajowka@gmail.com>
2021-01-29 04:29:56 +0200

    Auth routes restored

--- routes/api.js
+++ routes/api.js
@@ -201,8 +201,46 @@ router.delete(
   }
 );
 
-// create author - api/register
+// create author - api/signup
+router.post(
+  "/sign-up",
+  passport.authenticate("signup", { session: false }),
+  async (req, res, next) => {
+    res.json({
+      message: "Signup successful",
+      user: req.user,
+    });
+  }
+);
+
 // login - api/login
+router.post("/login", async (req, res, next) => {
+  passport.authenticate("login", async (err, user, info) => {
+    try {
+      if (err || !user) {
+        const error = new Error("An error occurred.");
+
+        return next(error);
+      }
+
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+
+        const body = { _id: user._id, username: user.username };
+        const token = jwt.sign({ user: body }, "TOP_SECRET");
+
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
+});
+
 // logout - api/logout
+router.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+});
 
 module.exports = router;
